caee9ecee57b557661400b4143f6ed3b910d805b
josele <joseluis.gomez.cano@hotmail.es>
2024-02-22 19:50:13 +0100

    repo

---
+++ src/pages/PokemonDetails/index.tsx
@@ -0,0 +1,11 @@
+
+
+type Props = {}
+
+const PokemonDetails = (props: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default PokemonDetails
---
+++ src/pages/PokemonList/index.tsx
@@ -0,0 +1,11 @@
+
+
+type Props = {}
+
+const PokemonList = (props: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default PokemonList
